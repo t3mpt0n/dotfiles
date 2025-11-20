d08b76ab45df275d3b164c19b2d596c3a677c117
t3mpt0n <git@t3mpt0n.com>
2025-11-20 06:53:10 -0500

    Fixed Emacs Org Mode

--- home/modules/editors/emacs/org.el
+++ home/modules/editors/emacs/org.el
@@ -1,8 +1,10 @@
-(leaf t3mpt0n/org-setup
+(leaf org
   :straight org-modern
+  :bind (("C-c a" . org-agenda))
   :hook
   (org-mode-hook . org-modern-mode)
   (org-agenda-finalize-hook . org-modern-agenda)
+  (org-mode-hook . turn-on-org-cdlatex)
   :setq
   (org-auto-align-tags . nil)
   (org-tags-column . 0)
@@ -12,28 +14,25 @@
   (org-hide-emphasis-markers . t)
   (org-pretty-entities . t)
   (org-agenda-tags-column . 0)
-  (org-ellipsis . "..."))
+  (org-ellipsis . "...")
+  (org-default-notes-file . "~/Documents/Org/Notes.org")
+
+  :config
+  (setq-default org-todo-keywords '((sequence "TODO(t)" "HOMEWORK(h)" "|" "DONE(d)" "CANCELLED(c)")))
+  (setq org-agenda-files '("~/Documents/Org/Agenda.org")))
 
 (leaf org-roam
   :straight t
-  :require org-roam-protocol
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-graph)
          ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ;; Dailies
-         ("C-c n j" . org-roam-dailies-capture-today))
+         (:org-mode-map
+          ("C-M-i" . completion-at-point)))
   :config
   (org-roam-setup)
-  (org-roam-db-autosync-mode)
   :custom
-  (org-roam-completion-everywhere . t)
   (org-roam-directory . "~/Documents/Org/Roam")
-  (org-roam-capture-templates . '(("d" "default" plain
-                                   "%?"
-                                   :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
-                                   :unnarrowed t))))
+  (org-roam-completion-everywhere . t))
 
 (leaf ob-mermaid
   :straight t
